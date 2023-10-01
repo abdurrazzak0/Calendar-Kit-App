fd741fc5dd95e5df565bd38dfe0e6411b2b41578
abdurrazzak0 <razzak6366@gmail.com>
2023-10-01 12:08:41 +0600

    update

--- Calendar Kit App/CalendarViewController.swift
+++ Calendar Kit App/CalendarViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Calendar Kit App/EKWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  EKWrapper.swift
+//  Calendar Kit App
+//
+//  Created by Abdur Razzak on 17/9/23.
+//
+
+import Foundation
